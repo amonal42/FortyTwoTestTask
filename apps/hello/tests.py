1ac2ab09c44369ee5a0e1b7c96ad0ae04c8c8b2a
Ievgenii Polishchuk <amonal42@gmail.com>
2015-02-20 01:42:55 +0200

    ticket#1 Changed test so it can be passed

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -2,9 +2,9 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 # Create your tests here.
-class BioViewTests(TestCase):
+class MainPageViewTests(TestCase):
     def test_index_view(self):
         response = self.client.get(reverse('hello:index'))
-        self.assertContains(response, "Name")
+        self.assertContains(response, "Bio")
         self.assertContains(response, "Contacts")
 
